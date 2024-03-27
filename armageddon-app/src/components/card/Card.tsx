2197225503ecde85f2079bdb90bcf3a0db13da89
JeksNEP <125944007+JeksNEP@users.noreply.github.com>
2024-03-27 10:45:26 +0300

    task 10

--- armageddon-app/src/components/card/Card.tsx
+++ armageddon-app/src/components/card/Card.tsx
@@ -19,12 +19,12 @@ export const AsteroidCard = (props:AsteroidCardProps) => {
 
     const {name, date, distance, size, isDangerous, distanceMode} = props;
 
-    return (<div className={styles.card}>
+    return <div className={styles.card}>
         <div className={isDangerous ? styles.cardRed : styles.normCard}>
             <AsteroidImage/>
             <AsteroidContent name={name} date={date} distance={distance} size={size} distanceMode={distanceMode}/>
-            <AsteroidAction isDangerous={isDangerous}/>
+            <AsteroidAction/>
         </div>
 
-    </div>)
+    </div>
 }
